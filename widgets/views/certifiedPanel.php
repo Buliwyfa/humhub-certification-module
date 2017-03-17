049c6ec98fc8680f28720c6aebeeaa0b72e2e5ca
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-17 12:30:32 -0400

    Update certifiedPanel.php

--- widgets/views/certifiedPanel.php
+++ widgets/views/certifiedPanel.php
@@ -16,7 +16,7 @@ $this->title = 'Certification';
             <?php echo Yii::t('CertifiedModule.widgets_views_certifiedPanel', 'You need to get certified as a real person in order to unlock some great features like email and picture gallery.
             Just upload a picture of yourself holding a sign with your screen name and the site name on it for every person in your profile. This is done as
             a security measure to protect our users identities as well as yours. No one will see the picture/pictures except for administrative personal, and will 
-            be deleted after verification has been approved. This is done to protect our users identities as well as yours.'); ?>
+            be deleted after verification has been approved. This is done to protect our users identities as well as yours.') ?>
         </p>
         <?= $this->render('_form', [
             'model' => $model,
