6313ca8789eee41c50ce5db7531e4d070bc4c2a5
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-17 12:20:39 -0400

    Create widgets_views_certifiedPanel.php

---
+++ messages/ja/widgets_views_certifiedPanel.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'You need to get certified as a real person in order to unlock some great features like email and picture gallery.
+            Just upload a picture of yourself holding a sign with your screen name and the site name on it for every person in your profile. This is done as
+            a security measure to protect our users identities as well as yours. No one will see the picture/pictures except for administrative personal, and will 
+            be deleted after verification has been approved. This is done to protect our users identities as well as yours.';';' => '',
+    
+];
