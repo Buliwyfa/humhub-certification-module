a9bdaa65ba5c8cb03a23b57498e68d3a40de5305
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-17 13:16:47 -0400

    Delete controllers_AwaitingCertificationController.php

--- messages/ja/controllers_AwaitingCertificationController.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'Needs Admin Approval' => '',
-    'Moved from Uncertified Group' => '',
-    'Something went wrong in the certified module change user groups in AwaitingCertificationController' => '',
-    'Could not save ' . $name . ' image. Please resubmit or notify admin.' => '',
-    
-];
