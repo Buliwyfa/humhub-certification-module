80fc18a4dda8b262f3d7f1baf9ad8914106b1e1b
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-17 13:04:44 -0400

    Update widgets_views_certifiedPanel.php

--- messages/en_US/widgets_views_certifiedPanel.php
+++ messages/en_US/widgets_views_certifiedPanel.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    '<strong>Get</strong> Certified' => '',
     'You need to get certified as a real person in order to unlock some great features like email and picture gallery.
             Just upload a picture of yourself holding a sign with your screen name and the site name on it for every person in your profile. This is done as
             a security measure to protect our users identities as well as yours. No one will see the picture/pictures except for administrative personal, and will 
